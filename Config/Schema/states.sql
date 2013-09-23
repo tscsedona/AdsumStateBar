73876b2c8f47402583e680932c2485379730c9e0
Chris Vogt <mail@chrisvogt.me>
2013-09-23 09:39:11 -0700

    Add schema to create `states` table.

---
+++ Config/Schema/states.sql
@@ -0,0 +1,57 @@
+CREATE  TABLE `adsum`.`states` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(50) NOT NULL ,
+  `abbreviation` CHAR(2) NOT NULL ,
+  PRIMARY KEY (`id`) );
+
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('1','Alabama','AL');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('2','Alaska','AK');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('3','Arizona','AZ');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('4','Arkansas','AR');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('5','California','CA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('6','Colorado','CO');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('7','Connecticut','CT');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('8','Delaware','DE');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('9','District of Columbia','DC');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('10','Florida','FL');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('11','Georgia','GA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('12','Hawaii','HI');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('13','Idaho','ID');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('14','Illinois','IL');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('15','Indiana','IN');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('16','Iowa','IA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('17','Kansas','KS');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('18','Kentucky','KY');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('19','Louisiana','LA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('20','Maine','ME');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('21','Maryland','MD');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('22','Massachusetts','MA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('23','Michigan','MI');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('24','Minnesota','MN');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('25','Mississippi','MS');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('26','Missouri','MO');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('27','Montana','MT');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('28','Nebraska','NE');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('29','Nevada','NV');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('30','New Hampshire','NH');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('31','New Jersey','NJ');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('32','New Mexico','NM');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('33','New York','NY');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('34','North Carolina','NC');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('35','North Dakota','ND');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('36','Ohio','OH');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('37','Oklahoma','OK');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('38','Oregon','OR');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('39','Pennsylvania','PA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('40','Rhode Island','RI');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('41','South Carolina','SC');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('42','South Dakota','SD');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('43','Tennessee','TN');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('44','Texas','TX');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('45','Utah','UT');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('46','Vermont','VT');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('47','Virginia','VA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('48','Washington','WA');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('49','West Virginia','WV');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('50','Wisconsin','WI');
+INSERT INTO `states` (`id`,`name`,`abbreviation`) VALUES ('51','Wyoming','WY');
--- View/StateBars/index.ctp
+++
@@ -1,3 +0,0 @@
-<h1>State Bars Index</h1>
-<p>Following is a sortable list of your contacts</p>
-<!-- A sortable list of contacts would go here....-->
